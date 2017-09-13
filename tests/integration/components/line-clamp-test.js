03bb7a8f5ce718552ef8f0ac5c3c37b10f14ceb9
Luis Torres <lui1423@gmail.com>
2017-09-13 15:48:11 -0700

    Adding integration test for component inline form

--- tests/integration/components/line-clamp-test.js
+++ tests/integration/components/line-clamp-test.js
@@ -5,21 +5,97 @@ moduleForComponent('line-clamp', 'Integration | Component | line clamp', {
   integration: true
 });
 
-test('it renders', function(assert) {
+test('inline form works as expeted', function(assert) {
 
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+  this.render(hbs`<div style="width: 300px; font-size: 16px; font-family: sans-serif;">
+    {{line-clamp
+      text="helloworld helloworld helloworld helloworld helloworld helloworld helloworld helloworld"
+    }}
+  </div>`);
 
-  this.render(hbs`{{line-clamp}}`);
+  const element = this.$()[0];
+  const lines = Array.from(element.querySelectorAll('.lt-line-clamp__line'));
+  const lastLine = lines[lines.length - 1];
+  const lastLineChildren = lastLine.children;
+  const ellipsisElement = lastLineChildren[0];
+  const seeMoreButton = ellipsisElement.children[0];
+  const dummyEllipsis = element.querySelectorAll('.lt-line-clamp--dummy');
 
-  assert.equal(this.$().text().trim(), '');
+  assert.ok(
+    element,
+    'line clamp target exists'
+  );
 
-  // Template block usage:
-  this.render(hbs`
-    {{#line-clamp}}
-      template block text
-    {{/line-clamp}}
-  `);
+  assert.equal(
+    lines.length,
+    3,
+    'text is clamped at 3 lines (default)'
+  );
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  // This test a bit flaky, depends on the width of div and knowing 'helloworld helloworld' will be a line
+  // assert.ok(
+  //   lines.slice(0,2)
+  //     .every((line) => line.innerText.trim() === 'helloworld helloworld'),
+  //   'first lines contain expected text'
+  // );
+
+  // This is a better test since we know the truncation will push 'helloworld' to a new line if it does not fit
+  assert.ok(
+    lines.slice(0,2)
+      .every((line) => line.innerText.trim().split(' ')[0] === 'helloworld'),
+    'first lines contain expected text'
+  );
+
+  assert.ok(
+    lastLine.classList.contains('lt-line-clamp__line--last'),
+    'lt-line-clamp__line--last is applied to last line'
+  );
+
+  assert.equal(
+    lastLineChildren.length,
+    1,
+    'last line contains 1 child'
+  );
+
+  assert.ok(
+    ellipsisElement,
+    'last line child is the ellipsis element and it exists'
+  );
+
+  assert.ok(
+    ellipsisElement.classList.contains('lt-line-clamp__ellipsis'),
+    'ellipsis element contains right CSS class'
+  );
+
+  assert.equal(
+    ellipsisElement.innerText,
+    '... See More',
+    'Ellipsis element contains expetend ellipsis and see more text'
+  );
+
+  assert.ok(
+    seeMoreButton,
+    'see more button exists'
+  );
+
+  assert.ok(
+    seeMoreButton.classList.contains('lt-line-clamp__more'),
+    'see more button contains right CSS class'
+  );
+
+  assert.equal(
+    seeMoreButton.innerText,
+    'See More',
+    'see more button contains expected text'
+  );
+
+  assert.ok(
+    dummyEllipsis,
+    'dummy ellipsis element exists'
+  );
+
+  assert.equal(
+    element.innerText.trim(),
+    'helloworld helloworld helloworld helloworld hellowor... See More'
+  );
 });
